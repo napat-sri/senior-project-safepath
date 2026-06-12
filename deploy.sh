d3a26585da784167b6c113be37d8a41f1c243398
Napat Sriwiroj <nasriwiroj@stud.mediadesign.de>
2026-06-12 23:15:17 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 cd /app
-git pull origin prod
+# git pull origin prod
+# 1. Fetch the latest updates from the remote repository
+git fetch origin prod
+
+# 2. Force-reset the local branch to match remote exactly (wipes out permission locks)
+git reset --hard origin/prod
+
+# 3. Clean up any untracked leftover files or build debris
+git clean -df
 docker compose up -d --build
 chmod +x deploy.sh
