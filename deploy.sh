8441e5f8d024c2b7b17105dbe9e2769fbd5bc7f9
Napat Sriwiroj <nasriwiroj@stud.mediadesign.de>
2026-06-13 00:16:40 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 cd /app
-git pull origin prod
+
+# 1. Fetch the latest history from the remote repository
+git fetch origin
+
+# 2. Force the local prod branch to exactly match the remote prod branch
+# This automatically wipes out any accidental local changes on the server!
+git reset --hard origin/prod
+
+# 3. Rebuild and restart the Docker containers
 docker compose up -d --build
-chmod +x deploy.sh
