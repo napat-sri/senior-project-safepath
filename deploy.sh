54f5b512be27752a9a58b2368e4ee86f2b014327
pantida-l <water.psu457@gmail.com>
2026-07-04 23:25:40 +0200

    refactor: clean up deploy script and improve output messages

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,26 @@
+# #!/bin/sh
+# cd /app
+
+# git fetch origin prod
+# git reset --hard origin/prod
+# git clean -fd
+
+# echo building project stack
+# docker compose up -d --build
+
+# echo building langflow stack
+# docker compose -f docker-compose-langflow.yml up -d --build
+
 #!/bin/sh
+set -eu
 cd /app
 
+echo "== git fetch =="
 git fetch origin prod
 git reset --hard origin/prod
 git clean -fd
+git log -1 --oneline        # print the commit you're now on
 
-echo building project stack
+echo "== build =="
 docker compose up -d --build
-
-echo building langflow stack
 docker compose -f docker-compose-langflow.yml up -d --build
-
