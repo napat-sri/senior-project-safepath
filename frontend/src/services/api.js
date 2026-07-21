2c8f4116a2160b71d8b15e25ae42a9742bbc9e62
Napat Sriwiroj <nasriwiroj@stud.mediadesign.de>
2026-07-21 22:18:34 +0200

    Implement Keycloak authentication integration and update environment configurations

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // keycloak
-// import keycloak from './keycloak'; 
+import keycloak from './keycloak'; 
 
 const apiBaseUrl =  process.env.VUE_APP_API_URL
 const api = axios.create({
@@ -9,17 +9,17 @@ const api = axios.create({
   headers: { 'Content-Type': 'application/json' },
 });
 // Add a fresh Keycloak token to every request when the user is logged in.
-// api.interceptors.request.use(async (config) => {
-//   if (keycloak.authenticated) {
-//     try {
-//       await keycloak.updateToken(30);
-//       config.headers.Authorization = `Bearer ${keycloak.token}`;
-//     } catch (err) {
-//       keycloak.login();
-//     }
-//   }
-//   return config;
-// });
+api.interceptors.request.use(async (config) => {
+  if (keycloak.authenticated) {
+    try {
+      await keycloak.updateToken(30);
+      config.headers.Authorization = `Bearer ${keycloak.token}`;
+    } catch (err) {
+      keycloak.login();
+    }
+  }
+  return config;
+});
 
 export const placeService = {
   search: (query) => api.get('/places', { params: { query } }),
--- frontend/src/services/keycloak.js
+++ frontend/src/services/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 // One shared Keycloak instance for the whole app.
 const keycloak = new Keycloak({
-  url: process.env.VUE_APP_KEYCLOAK_URL,            // http://localhost:8090
+  url: process.env.VUE_APP_KEYCLOAK_URL,            // http://localhost:8090 || https://login.safepath.duckdns.org
   realm: process.env.VUE_APP_KEYCLOAK_REALM,        // safepath
   clientId: process.env.VUE_APP_KEYCLOAK_CLIENT_ID, // safepath-frontend
 });
